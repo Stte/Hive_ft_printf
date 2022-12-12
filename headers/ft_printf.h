9b1fbc17cee5b39f587247cf8ea40289557a3669
Teemu Spoof <tspoof@student.hive.fi>
2022-12-12 23:37:20 +0200

    fixed libft include

--- headers/ft_printf.h
+++ headers/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: tspoof <tspoof@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 18:24:54 by tspoof            #+#    #+#             */
-/*   Updated: 2022/12/09 23:57:14 by tspoof           ###   ########.fr       */
+/*   Updated: 2022/12/12 22:45:51 by tspoof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <stdarg.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include "../libft/libft.h"
 
 char	*ft_convert_c(int c);
 char	*ft_convert_d(int c);
@@ -46,25 +47,5 @@ char	*ft_percent(void);
  * @return int Length of the returned string
  */
 int		ft_printf(const char *str, ...);
-
-//libft functions
-void	ft_bzero(void *s, size_t n);
-void	*ft_calloc(size_t count, size_t size);
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-char	*ft_itoa(int n);
-char	*ft_ltoa(long n);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memset(void *b, int c, int len);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strdup(const char *s1);
-char	*ft_strjoin(char const *s1, char const *s2);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlen(const char *s);
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-int		ft_toupper(int c);
-char	*ft_ulongtohex(unsigned long n);
 
 #endif
