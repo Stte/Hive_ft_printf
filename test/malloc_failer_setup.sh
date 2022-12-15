9319c21ae909138b94c0e07ba22cef4f1dbae12a
Teemu Spoof <tspoof@student.hive.fi>
2022-12-15 22:03:34 +0200

    fixed malloc_failer

--- test/malloc_failer_setup.sh
+++ test/malloc_failer_setup.sh
@@ -2,5 +2,5 @@
 
 docker build -t malloc-failer .
 docker run -dti --name printf_malloc_failer -v $(dirname $(pwd)):/project/ malloc-failer
-docker exec -ti printf_malloc_failer make linux
+docker exec -ti printf_malloc_failer make
 docker exec -ti printf_malloc_failer make malloc_failer -C ./test
