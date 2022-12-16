62ffe3d595781e1f8a84201ad3512f9df3b2221d
Teemu Spoof <tspoof@student.hive.fi>
2022-12-16 15:19:02 +0200

    removed duplicate doc string from ft_printf

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tspoof <tspoof@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 18:31:47 by tspoof            #+#    #+#             */
-/*   Updated: 2022/12/15 22:31:55 by tspoof           ###   ########.fr       */
+/*   Updated: 2022/12/16 15:06:10 by tspoof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,25 +89,6 @@ static int	ft_get_string(t_vec *result, const char *str, va_list args)
 	return (ret);
 }
 
-/**
- * @brief Print.
- *
- * Handles the following conversions: cspdiuxX%
- *
- * %c Prints a single character.
- * %s Prints a string (as defined by the common C convention).
- * %p The void * pointer argument has to be printed in hexadecimal format.
- * %d Prints a decimal (base 10) number.
- * %i Prints an integer in base 10.
- * %u Prints an unsigned decimal (base 10) number.
- * %x Prints a number in hexadecimal (base 16) lowercase format.
- * %X Prints a number in hexadecimal (base 16) uppercase format.
- * %% Prints a percent sign.
- *
- * @param str Text to print
- * @param ... Values to convert to string
- * @return int Length of the returned string
- */
 int	ft_printf(const char *str, ...)
 {
 	va_list	args;
